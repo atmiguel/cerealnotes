d44c9751fe430b5c5ac2c884ce52e7c3b10cace8
Adrian Miguel <adriantmiguel@gmail.com>
2018-03-06 16:47:33 -0500

    created super simple server

---
+++ index.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func rootHandler(
+        responseWriter http.ResponseWriter,
+        request *http.Request) {
+
+    var responseContent string = fmt.Sprintf(
+        "Hi there, I love %s!",
+        request.URL.Path[1:])
+
+    fmt.Fprintf(
+        responseWriter,
+        responseContent)
+}
+
+func main() {
+    // set handler
+    http.HandleFunc("/", rootHandler)
+
+    // start server
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
