44f994d22f7225d20d9842f0f5213aeeff44c791
Adrian Miguel <adriantmiguel@gmail.com>
2018-03-06 16:57:49 -0500

    added small thing to readme
    figuring out templates
    
    updated simple server
    
    renamed html file
    
    using simple template
    
    readme tweaks
    
    added build manifest
    
    more heroku magic
    
    updated readme
    
    updated simple server
    
    renamed html file
    
    using simple template
    
    readme tweaks
    
    added build manifest
    
    more heroku magic
    
    updated readme
    
    added static directory
    
    added MUI; mostly finished signup form
    
    cleaned up the html and css
    
    better front-end validation

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+    "html/template"
+    "log"
+    "net/http"
+    "os"
+    "strings"
+)
+
+func determineListenPort() (string, error) {
+  port := os.Getenv("PORT")
+
+  if port == "" {
+    return "", fmt.Errorf("$PORT not set")
+  }
+
+  return ":" + port, nil
+}
+
+func respondWithMethodNotAllowed(
+        responseWriter http.ResponseWriter,
+        allowedMethods []string) {
+
+    statusCode := http.StatusMethodNotAllowed
+
+    responseWriter.Header().Set(
+        "Allow",
+        strings.Join(allowedMethods, ", "))
+
+    http.Error(
+        responseWriter,
+        http.StatusText(statusCode),
+        statusCode)
+}
+
+func handleLoginOrSignupRequest(
+        responseWriter http.ResponseWriter,
+        request *http.Request) {
+
+    switch request.Method {
+        case http.MethodGet:
+            parsedTemplate, err := template.ParseFiles("templates/login_or_signup.tmpl")
+            if err != nil {
+                log.Fatal(err) // TODO is this right?
+            }
+
+            parsedTemplate.Execute(responseWriter, nil)
+
+        default:
+            respondWithMethodNotAllowed(
+                responseWriter,
+                []string{http.MethodGet})
+    }
+}
+
+func main() {
+    // SET ROUTER
+
+    // static files
+    {
+        staticDirectoryName := "static"
+        staticDirectoryPaddedWithSlashes := "/" + staticDirectoryName + "/"
+
+        fileServer := http.FileServer(http.Dir(staticDirectoryName))
+
+        http.Handle(
+            staticDirectoryPaddedWithSlashes,
+            http.StripPrefix(
+                staticDirectoryPaddedWithSlashes,
+                fileServer))
+    }
+
+    // templates
+    http.HandleFunc("/login-or-signup", handleLoginOrSignupRequest)
+
+    // START SERVER
+    port, err := determineListenPort()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    log.Printf("Listening on %s...\n", port)
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
+}
---
+++ static/js/login_or_signup.js
@@ -0,0 +1,107 @@
+var getInputField = function($form, field) {
+    return $form.find('[name="' + field + '"]');
+};
+
+var getInputFields = function($form, fields) {
+    return fields.map(
+        field => getInputField($form, field));
+};
+
+var checkFormValidity = function($form, fields) {
+    return getInputFields($form, fields).every(
+        $field => $field.get(0).checkValidity());
+};
+
+var getFormData = function($form, fields) {
+    return getInputFields($form, fields).reduce(
+        (formData, $field) => {
+            formData[$field.attr('name')] = $field.val();
+            return formData;
+        }, {});
+};
+
+var populateValidationMessage = function($field) {
+    var $validationMessage = $field.siblings('.validation-message').first();
+    var validationMessage = $field.get(0).validationMessage;
+
+    $validationMessage.text(validationMessage);
+};
+
+var populateValidationMessages = function($form, fields) {
+    getInputFields($form, fields).forEach(
+        $field => populateValidationMessage($field));
+};
+
+var touchAllFields = function($form, fields) {
+    getInputFields($form, fields).forEach($field => {
+        $field.focus().blur();
+    });
+};
+
+var checkKeypressIsSpace = function(event) {
+    return event.which === 32;
+};
+
+$(function() {
+    var $signupForm = $('#signup-form');
+
+    var submitHasBeenClicked = false;
+
+    var displayNameField = 'displayName';
+    var emailAddressField = 'emailAddress';
+    var passwordField = 'password';
+
+    var fields = [
+        displayNameField,
+        emailAddressField,
+        passwordField,
+    ];
+
+    getInputFields($signupForm, fields).forEach($field => {
+        var field = $field.attr('name');
+
+        // continuously update validation message after failed submission
+        $field.on('input', event => {
+            if (submitHasBeenClicked) {
+                populateValidationMessage($field);
+            }
+        });
+
+        if (field !== passwordField) {
+            // restrict inital space character
+            $field.keypress((event) => {
+                var value = $field.val();
+                var trimmedValue = $.trim(value);
+
+                if (checkKeypressIsSpace(event) && trimmedValue.length === 0) {
+                    return false;
+                }
+            });
+
+            // remove trailing spaces on blur
+            $field.blur(() => {
+                var value = $field.val();
+                var trimmedValue = $.trim(value);
+
+                $field.val(trimmedValue);
+            });
+        }
+    });
+
+    $signupForm.find('button').click(function() {
+	if (checkFormValidity($signupForm, fields)) {
+            $.post(
+                '/signup',
+                getFormData($signupForm, fields),
+                () => {
+                    console.log('done');
+                });
+
+        } else if (!submitHasBeenClicked) {
+            submitHasBeenClicked = true;
+
+            populateValidationMessages($signupForm, fields);
+            touchAllFields($signupForm, fields);
+        }
+    });
+});
