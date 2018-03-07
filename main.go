ce11a5733d43c9e1441b2fe56889b4e2fa239b01
Adrian Miguel <adriantmiguel@gmail.com>
2018-03-07 12:12:28 -0500

    added static directory

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
     "log"
     "net/http"
     "os"
+    "strings"
 )
 
 func determineListenPort() (string, error) {
@@ -18,24 +19,68 @@ func determineListenPort() (string, error) {
   return ":" + port, nil
 }
 
-func handleGetRootRequest(
+func respondWithMethodNotAllowed(
+        responseWriter http.ResponseWriter,
+        allowedMethods []string) {
+
+    statusCode := http.StatusMethodNotAllowed
+
+    responseWriter.Header().Set(
+        "Allow",
+        strings.Join(allowedMethods, ", "))
+
+    http.Error(
+        responseWriter,
+        http.StatusText(statusCode),
+        statusCode)
+}
+
+func handleLoginOrSignupRequest(
         responseWriter http.ResponseWriter,
         request *http.Request) {
 
-    // TODO handle error
-    parsedTemplate, _ := template.ParseFiles("root.tmpl")
-    parsedTemplate.Execute(responseWriter, nil)
+    switch request.Method {
+        case http.MethodGet:
+            parsedTemplate, err := template.ParseFiles("templates/login_or_signup.tmpl")
+            if err != nil {
+                log.Fatal(err) // TODO is this right?
+            }
+
+            parsedTemplate.Execute(responseWriter, nil)
+
+        default:
+            respondWithMethodNotAllowed(
+                responseWriter,
+                []string{http.MethodGet})
+    }
 }
 
 func main() {
+    // SET ROUTER
+
+    // static files
+    {
+        staticDirectoryName := "static"
+        staticDirectoryPaddedWithSlashes := "/" + staticDirectoryName + "/"
+
+        fileServer := http.FileServer(http.Dir(staticDirectoryName))
+
+        http.Handle(
+            staticDirectoryPaddedWithSlashes,
+            http.StripPrefix(
+                staticDirectoryPaddedWithSlashes,
+                fileServer))
+    }
+
+    // templates
+    http.HandleFunc("/login-or-signup", handleLoginOrSignupRequest)
+
+    // START SERVER
     port, err := determineListenPort()
     if err != nil {
         log.Fatal(err)
     }
 
-    http.HandleFunc("/", handleGetRootRequest) // set router
-
-    // start server
     log.Printf("Listening on %s...\n", port)
 
     if err := http.ListenAndServe(port, nil); err != nil {
