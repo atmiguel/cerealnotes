1b6b56fc162b112a4e89be3eb7ad1be054ab2b72
Adrian Miguel <adriantmiguel@gmail.com>
2018-03-08 01:00:59 -0500

    added MUI; mostly finished signup form

--- static/js/login_or_signup.js
+++ static/js/login_or_signup.js
@@ -1,1 +1,71 @@
-alert('loaded js');
+var getInputFields = function($form, fields) {
+    return fields.map(
+        field => $form.find('[name="' + field + '"]'));
+};
+
+var checkFormValidity = function($form, fields) {
+    return getInputFields($form, fields).every(
+        $field => $field.get(0).checkValidity());
+};
+
+var getFormData = function($form, fields) {
+    return getInputFields($form, fields).reduce(
+        (formData, $field) => {
+            formData[$field.attr('name')] = $field.val();
+            return formData;
+        }, {});
+};
+
+var populateValidationMessage = function($field) {
+    var $validationMessage = $field.siblings('.validation-message').first();
+    var validationMessage = $field.get(0).validationMessage;
+
+    $validationMessage.text(validationMessage);
+};
+
+var populateValidationMessages = function($form, fields) {
+    getInputFields($form, fields).forEach(
+        $field => populateValidationMessage($field));
+};
+
+var touchAllFields = function($form, fields) {
+    getInputFields($form, fields).forEach($field => {
+        $field.focus().blur();
+    });
+};
+
+$(function() {
+    // Signup Form
+    var $signupForm = $('#signup-form');
+    var submitHasBeenClicked = false;
+
+    var fields = [
+        'displayName',
+        'emailAddress',
+        'password',
+    ];
+
+    getInputFields($signupForm, fields).forEach($field => {
+        $field.on('input', () => {
+            if (submitHasBeenClicked) {
+                populateValidationMessage($field);
+            }
+        });
+    });
+
+    $signupForm.find('button').click(function() {
+        submitHasBeenClicked = true;
+
+	if (checkFormValidity($signupForm, fields)) {
+            $.post(
+                '/signup',
+                getFormData($signupForm, fields),
+                () => {
+                    console.log('done');
+                });
+        } else {
+            populateValidationMessages($signupForm, fields);
+            touchAllFields($signupForm, fields);
+        }
+    });
+});
