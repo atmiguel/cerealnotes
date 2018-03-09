e585fa168a7c7c064f99e56e3dbf93e0d87682f7
Giuseppe <zingales@mit.edu>
2018-03-09 00:17:58 -0800

    added all three tables

--- migrations/0000_createDbs.sql
+++ migrations/0000_createDbs.sql
@@ -5,3 +5,20 @@ CREATE TABLE IF NOT EXISTS "Users"(
     password bytea NOT NULL,
     "creationTime" timestamp NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS "Publications"(
+	id bigserial PRIMARY KEY,
+    "authorId" bigint references "Users"(id) NOT NULL,
+    "sequenceNumber" serial NOT NULL,
+	"creationTime" timestamp NOT NULL
+
+);
+
+CREATE TABLE IF NOT EXISTS "Notes"(
+	id bigserial PRIMARY KEY,
+    "authorId" bigint references "Users"(id) NOT NULL,
+    "type" text,
+    "content" text NOT NULL,
+    "publicationId" bigint references "Publications"(id),
+    "creationTime" timestamp NOT NULL
+);
