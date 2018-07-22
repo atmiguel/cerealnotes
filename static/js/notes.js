3255e3888c0db5604419ce5437a582893bafc220
Adrian Miguel <adriantmiguel@gmail.com>
2018-07-22 19:53:36 -0400

    got buttons to show up

--- static/js/notes.js
+++ static/js/notes.js
@@ -38,7 +38,56 @@ const $createNote = function(note) {
         .append($content);
 };
 
+const $createButtonWithText = function(text) {
+    return $('<button>').addClass('mui-btn').text(text);
+};
+
+const $createHalfRowDivWithElement = function($element) {
+    return $('<div>').addClass('mui-col-xs-6').append($element);
+}
+
+const $createRowWithTwoElements = function($element1, $element2) {
+    const $row = $('<div>').addClass('mui-row');
+
+    const $column1 = $createHalfRowDivWithElement($element1);
+    const $column2 = $createHalfRowDivWithElement($element2);
+
+    return $row
+        .append($column1)
+        .append($column2);
+};
+
+const $createGridContainer = function() {
+    return $('<div>').addClass('mui-container-fluid');
+};
+
+const $createAddNoteModal = function() {
+    const $modal = $('<div>').addClass('modal');
+
+    const noteTypes = [
+        'Marginalia',
+        'Meta',
+        'Prediction',
+        'Question',
+    ];
+
+    const $buttons = noteTypes.map(noteType => {
+        return $createButtonWithText(noteType).addClass('note-type-button');
+    });
+
+    return $modal
+        .append($createGridContainer()
+            .append($createRowWithTwoElements($buttons[0], $buttons[1]))
+            .append($createRowWithTwoElements($buttons[2], $buttons[3])));
+};
+
+const activateModal = function($modal) {
+    mui.overlay('on', $modal.get(0));
+}
+
 $(function() {
+    const $addNoteModal = $createAddNoteModal();
+
     $.get('/api/user', function(usersById) {
         USERS_BY_ID = usersById;
 
@@ -51,5 +100,9 @@ $(function() {
                 );
             });
         });
+    });
+
+    $('#add-note-button').click(function() {
+        activateModal($addNoteModal);
     });
 });
