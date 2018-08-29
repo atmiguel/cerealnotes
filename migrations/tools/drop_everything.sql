7e4c0b9e3681264862852ba1435e415c28604442
G <git@zingales.org>
2018-08-29 10:58:56 -0700

    delete api`

--- migrations/tools/drop_everything.sql
+++ migrations/tools/drop_everything.sql
@@ -6,6 +6,6 @@ DROP TABLE publication CASCADE;
 
 DROP TABLE note CASCADE;
 
-DROP TABLE note_to_type_relationship CASCADE;
+DROP TABLE note_to_category_relationship CASCADE;
 
 DROP TABLE note_to_publication_relationship CASCADE;
