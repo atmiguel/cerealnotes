07c8299afaf13044819dff06281a64107147976f
G <git@zingales.org>
2018-09-17 10:15:55 -0700

    added final db test, also have the test clear the Db before they run

--- models/datastore_test.go
+++ models/datastore_test.go
@@ -1,29 +1,56 @@
 package models_test
 
 import (
+	"fmt"
+	"strconv"
 	"testing"
 	"time"
-	"strconv"
 
 	"github.com/atmiguel/cerealnotes/models"
 )
 
 var postgresUrl = "postgresql://localhost/test_db?sslmode=disable"
 
-func ClearAllValuesInTable(*models.DB) {
-	// todo call trucate_tables.sql
+var tables = []string{
+	"note_to_publication_relationship",
+	"publication",
+	"note_to_category_relationship",
+	"note",
+	"app_user",
+}
+
+func ClearAllValuesInTable(db *models.DB) {
+	for _, val := range tables {
+		if err := ClearValuesInTable(db, val); err != nil {
+			panic(err)
+		}
+	}
+
+}
+
+func ClearValuesInTable(db *models.DB, table string) error {
+	// db.Query() doesn't allow varaibles to replace columns or table names.
+	sqlQuery := fmt.Sprintf(`TRUNCATE %s CASCADE;`, table)
+
+	_, err := db.Exec(sqlQuery)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func TestUser(t *testing.T) {
 	db, err := models.ConnectToDatabase(postgresUrl)
 	ok(t, err)
+	ClearAllValuesInTable(db)
 
 	displayName := "boby"
 	password := "aPassword"
 	emailAddress := models.NewEmailAddress("thisIsMyOtherEmail@gmail.com")
 
-	err = db.StoreNewUser(displayName,emailAddress,password)
-	ok(t, err) 
+	err = db.StoreNewUser(displayName, emailAddress, password)
+	ok(t, err)
 
 	_, err = db.GetIdForUserWithEmailAddress(emailAddress)
 	ok(t, err)
@@ -35,13 +62,14 @@ func TestUser(t *testing.T) {
 func TestNote(t *testing.T) {
 	db, err := models.ConnectToDatabase(postgresUrl)
 	ok(t, err)
+	ClearAllValuesInTable(db)
 
 	displayName := "bob"
 	password := "aPassword"
 	emailAddress := models.NewEmailAddress("thisIsMyEmail@gmail.com")
 
-	err = db.StoreNewUser(displayName,emailAddress,password)
-	ok(t, err) 
+	err = db.StoreNewUser(displayName, emailAddress, password)
+	ok(t, err)
 
 	userId, err := db.GetIdForUserWithEmailAddress(emailAddress)
 	ok(t, err)
@@ -50,4 +78,27 @@ func TestNote(t *testing.T) {
 	id, err := db.StoreNewNote(note)
 	ok(t, err)
 	assert(t, int64(id) > 0, "Note Id was not a valid index: "+strconv.Itoa(int(id)))
+}
+
+func TestCategory(t *testing.T) {
+	db, err := models.ConnectToDatabase(postgresUrl)
+	ok(t, err)
+	ClearAllValuesInTable(db)
+
+	displayName := "bob"
+	password := "aPassword"
+	emailAddress := models.NewEmailAddress("thisyetAnotherIsMyEmail@gmail.com")
+
+	err = db.StoreNewUser(displayName, emailAddress, password)
+	ok(t, err)
+
+	userId, err := db.GetIdForUserWithEmailAddress(emailAddress)
+	ok(t, err)
+
+	note := &models.Note{AuthorId: userId, Content: "I'm a note", CreationTime: time.Now()}
+	noteId, err := db.StoreNewNote(note)
+	ok(t, err)
+
+	err = db.StoreNewNoteCategoryRelationship(noteId, models.META)
+	ok(t, err)
 }
