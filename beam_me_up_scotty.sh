b7e12a1b7445ae4cc17a968bc36799674c6a1ec9
G <git@zingales.org>
2018-09-23 08:36:06 -0700

    evan wants bash

--- beam_me_up_scotty.sh
+++ beam_me_up_scotty.sh
@@ -1,2 +1,16 @@
-(docker-compose --file docker-compose.dev.yml up -d) && docker exec -it cerealnotes_backend_1 bash -c 'go run main.go' && (docker-compose --file docker-compose.dev.yml down)
-# docker exec -it cerealnotes_backend_1 /bin/bash
+#! /bin/bash
+
+exit_script() {
+    echo "Scotty: Preparing for next beam"
+    docker-compose --file docker-compose.dev.yml down;
+}
+
+trap exit_script SIGINT SIGTERM
+
+docker-compose --file docker-compose.dev.yml up -d;
+if [ "$1" = "bash" ]; then
+	docker exec -it cerealnotes_backend_1 /bin/bash;
+	docker-compose --file docker-compose.dev.yml down;
+else
+	docker exec cerealnotes_backend_1 bash -c 'go run main.go';
+fi
