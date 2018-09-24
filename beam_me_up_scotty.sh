b70b31f83818be8b0c6d8d04fa23108e5d6e9190
G <git@zingales.org>
2018-09-24 12:36:19 -0700

    readme cuteness++

--- beam_me_up_scotty.sh
+++ beam_me_up_scotty.sh
@@ -5,7 +5,7 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 exit_script() {
-	echo -e "${GREEN}Shutting down beam, sir!${NC}"
+	echo -e "${GREEN}As you command, sir!${NC}"
 }
 
 trap exit_script SIGINT SIGTERM
@@ -19,7 +19,7 @@ elif [ "$1" = "bash" ]; then
 	echo -e "${GREEN}Beaming you straight into quantum space, sir!${NC}"
 	docker exec -it cerealnotes_backend_1 /bin/bash;
 else
-	echo -e "${GREEN}Validating tele-ions and then the beam will start, sir!${NC}"
+	echo -e "${GREEN}Engaging wormhole stabalizers. Beam will start shortyly, sir!${NC}"
 	echo "Running tests then staring the service"
 	docker exec cerealnotes_backend_1 bash -c 'go test ./... && go run main.go';
 fi
